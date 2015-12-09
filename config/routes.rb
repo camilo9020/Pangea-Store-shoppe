a3f6b2ff0261ac97333bc187ce8318d4c1c787c6
camilo9020 <camilo9020@gmail.com>
2015-12-09 17:52:32 -0500

    Se agregan las vistas de about, portafolio, contacto y blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'pages/about'
+  get 'pages/portafolio'
+  get 'pages/contacto'
+  get 'pages/blog'
 
   mount Shoppe::Engine => "/shoppe"
   # The priority is based upon order of creation: first created -> highest priority.
