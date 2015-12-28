f37c06ab57f55b0cee081716e9ca013db5d3b566
juanfevargax2 <juanfevargax2@gmail.com>
2015-12-28 12:38:07 -0500

    home: filtros basicos funcionando

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,7 +33,24 @@ module ApplicationHelper
       "shoppe_product_translations"."permalink" LIKE ? OR 
       "shoppe_product_category_translations"."name" LIKE ? OR 
       "shoppe_product_category_translations_parent"."name" LIKE ?',search_input,search_input,search_input,search_input,search_input)
+  end
+  
+  def filterCategory(products,params)
+     
+     products.joins('LEFT OUTER JOIN "shoppe_product_category_translations" ON "shoppe_product_category_translations"."id" = "shoppe_product_categories"."id" 
+     LEFT OUTER JOIN "shoppe_product_category_translations" "shoppe_product_category_translations_parent" ON "shoppe_product_category_translations_parent"."id" = "shoppe_product_categories"."parent_id" 
+     ')
+     .where('shoppe_product_category_translations.name IN (:categories) OR shoppe_product_category_translations_parent.name IN (:categories)',{categories: params[:categories]})
 
+  end
+  
+  def filterBrands(products,params)
+      products.joins('LEFT OUTER JOIN "shoppe_product_attributes" ON "shoppe_product_attributes"."product_id" = "shoppe_products"."id"')
+      .where('shoppe_product_attributes.value IN (:brands) AND shoppe_product_attributes.key == "Marca"',{brands: params[:brands]})  
+  end
+  
+  def filterPrice(products,params)
+      products.where('(shoppe_products.price BETWEEN :min AND :max AND variants_shoppe_products.price IS NULL ) OR (variants_shoppe_products.price BETWEEN :min AND :max ) ',{min: params[:minPrice] , max: params[:maxPrice]})  
   end
   
 end
